23b5860cdebec8ca9c5f06e43468119b78454a6e
vadim.ush <vadim.ush@eb903ba2-5a3e-11de-b96a-e1c1f561d0d2>
2009-07-30 18:58:49 +0000

    Add cmd line options --show-parse-progress and --optimize. Some changes in build script.
    git-svn-id: http://context2.googlecode.com/svn@21 eb903ba2-5a3e-11de-b96a-e1c1f561d0d2

--- lcontext/trunk/src/make.bat
+++ lcontext/trunk/src/make.bat
@@ -7,19 +7,21 @@ call :mk out\lcontext_a.exe ctx4win       --win32-c out\ lcontext_b.exe
 call :mk out\lcontext_b.exe ctx4win       --win32-c out\ lcontext_c.exe
 call :mk out\lcontext_c.exe ctx4lnx       --linux   out\ lcontext_c
 
+call :mk out\lcontext_c.exe ctx4win       --win32-c out\ lcontext_c_debug.exe --optimize none
+
 call :mk out\lcontext_c.exe samples\z_t1  --win32-c out\ z_t1.exe
 call :mk out\lcontext_c.exe samples\z_t2  --win32-w out\ z_t2.exe
 call :mk out\lcontext_c.exe samples\z_t3  --win32-c out\ z_t3.exe
 call :mk out\lcontext_c.exe samples\z_t4  --win32-c out\ z_t4.exe
 rem out\z_t4.exe > out\z_t4_1.ctx
-rem call :mk out\lcontext_c.exe out\z_t4_1    --win32-c out\ z_t4_1.exe
+rem call :mk out\lcontext_c.exe out\z_t4_1    --win32-c out\ z_t4_1.exe --show-parse-progress
 
 goto :end
 
 
 :mk
 echo %2 ==^> %4%5
-%1    %2.ctx %3 --output %4.%5.asm --output-tree %4.%5.tree --output-include-list %4.%5.includes
+%1    %2.ctx %3 --output %4.%5.asm --output-tree %4.%5.tree --output-include-list %4.%5.includes %6 %7 %8 %9
 if not %ERRORLEVEL%==0 goto err
 fasm  %4.%5.asm %4%5 > nul
 if not %ERRORLEVEL%==0 goto err
